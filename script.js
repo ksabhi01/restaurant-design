a08edcb8adc3732c6ae9ed6de1455c1b52734ef2
ksabhi01 <ksabhishek639@gmail.com>
2020-04-01 18:45:45 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,125 @@
+$(document).ready(function(){
+	$("body").css({
+		"margin":"0",
+		"padding": "0",
+		"font-family" : "calluna-n4, calluna, serif"
+	});
+	
+	$("#main_heading").css({
+		"background": "url('./media/food-composition.jpg')",
+		"background-position": "center",
+		"background-size" : "cover",
+		"height": "100vh",
+		
+	});
+	
+	$("#logo").css({
+		"height" : "3.5rem",
+		"width" : "auto",
+		"padding" : "1em"
+	});
+	
+	$("li").css({
+		"list-style-type" : "none",
+		
+		
+	});
+	
+	$("a").css({
+		"text-decoration" : "none",
+		"color" : "white"
+	});
+	
+	$(".nav_links").css({
+		"padding" : "15px",
+		"margin-top" : "10px"
+		
+	});
+	
+	$("#footer").css({
+		"margin-top" : "98px",
+		"font-size" : "25px",
+		"color" : "white"
+	});
+	
+	$("ul").css({
+		"background" : "rgba(15,15,13,0.8)",
+		"width" : "17vw",
+		"height" : "100vh",
+		"margin" : "0",
+		"position" : "fixed"
+	});
+	
+	$("#book").css({
+		"background" : "rgba(15,15,13,0.9)",
+		"width" : "88%"
+	});
+	
+	$("#subscribe").css({
+		"position" : "absolute",
+		"top" : "5%",
+		"right" : "3%",
+		"width" : "13%",
+		"height" : "auto"
+	});
+	
+	$("#pattern").css({
+		"padding": "14px",
+		"position": "relative",
+		"top": "45%",
+		"transform": "translateY(-50%)",
+		"margin": "0 auto",
+		"max-width": "540px"
+	});
+	
+	$("#middle_quote").css({
+		"position" : "absolute",
+		"width" : "33.75rem",
+		"height" : "15.06rem",
+		"padding" : "10px 20px 0.7em",
+		"background" : "rgba(0,0,0,0.8)",
+		"opacity" : "1",
+		"top" : "28%",
+		"left" : "38%"
+	});
+	
+	$("#middle_quote h1").css({
+		"color" : "#C9C1AB",
+		"text-align" : "center",
+		"font-weight" : "700",
+		"font-size" : "2.2em"
+	});
+	
+	$("#menu_button").css({
+		"background" : "transparent",
+		"color" : "#DB8759",
+		"border" : "1px solid #DB8759",
+		"padding" : "16px 25px",
+		"font-weight" : "900",
+		"margin-top" : "22px",
+		"margin-left" : "34%",
+		"cursor" : "pointer"
+	});
+	
+	$("#main").css({
+		"background" : "url('./media/background.jpg')",
+		"height" : "70vh",
+		"width" : "auto",
+		"background-position": "center",
+		"background-size" : "cover",
+		"background-attachment" : "fixed",
+	});
+	
+	$("#main>h1, #main>h2").css({
+		"color" : "white",
+		"text-align" : "center"
+	});
+	
+	
+	
+	
+	
+	
+	
+
+});
